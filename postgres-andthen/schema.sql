1f94979dd8e4692ca53aa923f7eb9bda71380060
Tyler Darnell <tyler.a.darnell@gmail.com>
2022-04-12 17:40:49 -0500

    Add players microservice
    Add players microservice table in schema.sql. Add backend to haproxy. Add image info to docker-compose.
    
    Additionally add admin field to user database.

--- postgres-andthen/schema.sql
+++ postgres-andthen/schema.sql
@@ -5,10 +5,22 @@ CREATE DATABASE andthen;
 CREATE TABLE users (
 	id serial PRIMARY KEY,
 	username varchar NOT NULL UNIQUE,
-	password varchar NOT NULL
+	password varchar NOT NULL,
+	admin boolean
 );
 
 CREATE ROLE andthen_auth LOGIN;
 ALTER ROLE andthen_auth WITH PASSWORD 'temporary_password';
 
 ALTER TABLE users OWNER TO andthen_auth;
+
+CREATE TABLE players (
+	id serial PRIMARY KEY,
+	display_name varchar,
+	user_id int NOT NULL
+);
+
+CREATE ROLE andthen_players LOGIN;
+ALTER ROLE andthen_players WITH PASSWORD 'temporary_password';
+
+ALTER TABLE players OWNER TO andthen_players;
